88db53fdeae9a4ff2c23060a00a25e2387d44842
주민재 <john_joo7@naver.com>
2024-03-12 14:41:56 +0900

    boj 169575

--- boj_solve/src/boj_solve.cpp
+++ boj_solve/src/boj_solve.cpp
@@ -1,68 +1,100 @@
 #include <bits/stdc++.h>
 
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
 #define endl "\n"
 
+#define FASTIO std::ios_base::sync_with_stdio(false);std::cin.tie(0);std::cout.tie(0)
+
+#define GET(A,B) (A[B.first][B.second])
+
 using namespace std;
-using idp = pair<int, double>;
-using ddp = pair<double, double>;
 
-vector<vector<idp>> edges;
-vector<ddp> volts;
-vector<bool> visi;
+using lld = long long int;
+using iip = pair<int, int>;
+using iiit = tuple<int, int, int>;
+using iilt = tuple<int, int, lld>;
+using lllt = tuple<lld, lld, lld>;
+using llp = pair<lld, lld>;
 
-class Func {
-public:
-	bool operator() (idp& a, idp& b) { return a.second > b.second; }
-};
 
-inline double getDis(ddp& a, ddp& b) {
-	return sqrt(pow(a.first - b.first, 2) + pow(a.second - b.second, 2));
+int N, M;
+lld arr[1000001];
+lld seg[4000004];
+lld lazy[4000004];
+
+lld make(int low, int hi, int idx) {
+    if (low == hi)
+        return seg[idx] = arr[low];
+    int mid = (low + hi) / 2;
+    return seg[idx] = (make(low, mid, idx * 2) + make(mid + 1, hi, idx * 2 + 1));
+}
+
+void updateLazy(int low, int hi, int idx) {
+    if (lazy[idx] == 0) return;
+
+    seg[idx] += lazy[idx] * (hi - low + 1);
+    lazy[idx * 2] += lazy[idx];
+    lazy[idx * 2 + 1] += lazy[idx];
+    lazy[idx] = 0;
+}
+
+lld update(int ilow, int ihi, lld n, int low, int hi, int idx) {
+    updateLazy(low, hi, idx);
+
+    if (low == ilow && hi == ihi) {
+        lazy[idx * 2] += n;
+        lazy[idx * 2 + 1] += n;
+        return seg[idx] += n * (hi - low + 1);
+    }
+
+    int mid = (low + hi) / 2;
+
+    if (ihi <= mid) return seg[idx] = update(ilow, ihi, n, low, mid, idx * 2);
+    else if (mid < ilow) return seg[idx] = update(ilow, ihi, n, mid + 1, hi, idx * 2 + 1);
+    return seg[idx] = update(ilow, mid, n, low, mid, idx * 2) + update(mid + 1, ihi, n, mid + 1, hi, idx * 2 + 1);
+}
+
+
+lld get(int i, int low, int hi, int idx) {
+    updateLazy(low, hi, idx);
+    if (hi == low) return seg[idx];
+    int mid = (low + hi) / 2;
+    if (i <= mid) return get(i, low, mid, idx * 2);
+    else return get(i, mid + 1, hi, idx * 2 + 1);
 }
 
 int main() {
-	FASTIO;
+    FASTIO;
 
-	int N;
+    cin >> N;
 
-	cin >> N;
+    for (int i = 1; i <= N; ++i) {
+        cin >> arr[i];
+    }
 
-	volts.resize(N);
-	visi.resize(N, false);
-	edges.resize(N);
+    make(1, N, 1);
 
-	for(int i = 0; i < N; ++i) {
-		cin >> volts[i].first >> volts[i].second;
+    cin >> M;
 
-		for(int j = 0; j < i; ++j) {
-			edges[i].push_back({ j, getDis(volts[i], volts[j])});
-			edges[j].push_back({ i, getDis(volts[i], volts[j])});
-		}
-	}
+    for (int i = 0; i < M; ++i) {
+        int cmd;
+        cin >> cmd;
 
-	double ans = 0;
-	priority_queue<idp, vector<idp>, Func> pq;
-	pq.push({0, 0});
+        if (cmd == 1) {
+            int a, b;
+            lld c;
 
-	for(int cnt = 0; cnt < N; ++cnt) {
-		auto [cur, len] = pq.top();
-		pq.pop();
+            cin >> a >> b >> c;
 
-		if(visi[cur]) { cnt--; continue; }
+            update(a, b, c, 1, N, 1);
+        }
+        else {
+            int a;
 
-		visi[cur] = true;
-		ans += len;
+            cin >> a;
 
-		for(auto& [n, l] : edges[cur]) {
-			if(visi[n]) continue;
-			pq.push({ n, l });
-		}
-	}
+            cout << get(a, 1, N, 1) << endl;
+        }
+    }
 
-	cout.precision(2);
-	cout << fixed;
-
-	cout << ans;
-
-	return 0;
+    return 0;
 }
