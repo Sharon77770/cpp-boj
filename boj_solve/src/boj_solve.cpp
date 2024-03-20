35603f564cb8dabecaf4893798e0e58e0bfe09b8
주민재 <john_joo7@naver.com>
2024-03-20 11:19:17 +0900

    boj 14268

--- boj_solve/src/boj_solve.cpp
+++ boj_solve/src/boj_solve.cpp
@@ -4,8 +4,6 @@
 
 #define FASTIO std::ios_base::sync_with_stdio(false);std::cin.tie(0);std::cout.tie(0)
 
-#define GET(A,B) (A[B.first][B.second])
-
 using namespace std;
 
 using lld = long long int;
@@ -15,57 +13,97 @@ using iilt = tuple<int, int, lld>;
 using lllt = tuple<lld, lld, lld>;
 using llp = pair<lld, lld>;
 
+const int MAX_LEN = 600010;
+
+lld seg[MAX_LEN];
+lld lazy[MAX_LEN];
+vector<int> nodeName;
+vector<int> section;
+vector<vector<int>> graph;
+int M, N, root;
+
+void renameNode(int cur, int& n) {
+	nodeName[cur] = n++;
+	for(auto& nxt : graph[cur])
+		renameNode(nxt, n);
+
+	section[cur] = n - 1;
+}
+
+void updateLazy(int low, int hi, int idx) {
+	if(lazy[idx] == 0) return;
+
+	seg[idx] += lazy[idx] * (hi - low + 1);
+	lazy[idx * 2] += lazy[idx];
+	lazy[idx * 2 + 1] += lazy[idx];
+	lazy[idx] = 0;
+}
+
+lld updateSeg(int ilow, int ihi, lld w, int low, int hi, int idx) {
+	updateLazy(low, hi, idx);
+
+	if(ilow == low && ihi == hi) {
+		lazy[idx * 2] += w;
+		lazy[idx * 2 + 1] += w;
+		return seg[idx] += w * (hi - low + 1);
+	}
+
+	int mid = (low + hi) / 2;
+
+	if(ihi <= mid) return seg[idx] = updateSeg(ilow, ihi, w, low, mid, idx * 2);
+	else if(ilow > mid) return seg[idx] = updateSeg(ilow, ihi, w, mid + 1, hi, idx * 2 + 1);
+	return seg[idx] = updateSeg(ilow, mid, w, low, mid, idx * 2) +
+			updateSeg(mid + 1, ihi, w, mid + 1, hi, idx * 2 + 1);
+}
+
+lld getData(int targ, int low, int hi, int idx) {
+	updateLazy(low, hi, idx);
+
+	if(low >= hi) return seg[idx];
+
+	int mid = (low + hi) / 2;
+
+	if(targ <= mid) return getData(targ, low, mid, idx * 2);
+	else return getData(targ, mid + 1, hi, idx * 2 + 1);
+}
+
 int main() {
     FASTIO;
 
-    deque<int> st;
-    int N, n;
+    cin >> N >> M;
 
-    cin >> N;
+    graph.resize(N + 1);
+    section.resize(N + 1);
+    nodeName.resize(N + 1);
 
-    while (N--) {
-    	int cmd;
+    for (int i = 1; i <= N; ++i) {
+    	int parent;
 
+    	cin >> parent;
+
+    	if(parent != -1) graph[parent].push_back(i);
+    }
+
+    int cmd = 1;
+    renameNode(1, cmd);
+
+    while (M--) {
     	cin >> cmd;
 
-    	switch (cmd) {
-    	case 1:
-    		cin >> n;
-    		st.push_front(n);
-    		break;
+    	if (cmd == 1) {
+    		int i;
+    		lld w;
 
-    	case 2:
-    		cin >> n;
-    		st.push_back(n);
-    		break;
+    		cin >> i >> w;
 
-    	case 3:
-    		if (st.empty()) cout << -1 << endl;
-    		else { cout << st.front() << endl; st.pop_front(); }
-    		break;
+    		updateSeg(nodeName[i], section[i], w, 1, N, 1);
+    	}
+    	else {
+    		int i;
 
-    	case 4:
-    		if (st.empty()) cout << -1 << endl;
-    		else { cout << st.back() << endl; st.pop_back(); }
-    		break;
+    		cin >> i;
 
-    	case 5:
-    		cout << st.size() << endl;
-    		break;
-
-    	case 6:
-    		cout << (int)st.empty() << endl;
-    		break;
-
-    	case 7:
-    		if (st.empty()) cout << -1 << endl;
-    		else cout << st.front() << endl;
-    		break;
-
-    	default:
-    		if (st.empty()) cout << -1 << endl;
-    		else cout << st.back() << endl;
-    		break;
+    		cout << getData(nodeName[i], 1, N, 1) << endl;
     	}
     }
 
